c0b9266fb5a055d224c6f07d21a4ae5dc47bae90
Chris Eichler <chriseichler@gmail.com>
2013-08-12 01:35:09 -0400

    put in more content including screenshot and link of new lost tribes-made basically presentable. Aug 12 2013

--- portfolio.js
+++ portfolio.js
@@ -16,14 +16,14 @@
 $(function() {
 
   $('#objectb').on('click', function() {
-    $('#maincontainer').fadeOut(1000, function() {
+    $('#maincontainer').fadeOut(900, function() {
       $('#contactinfo').fadeIn().css('display','inline-block');
     });
   });
 
 
   $('#contactinfo').on('click', function() {
-    $('#contactinfo').fadeOut(1000, function() {
+    $('#contactinfo').fadeOut(900, function() {
       $('#maincontainer').fadeIn();
     });
   });
