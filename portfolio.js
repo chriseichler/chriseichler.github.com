6519d1b6052b9496523695c1ca449e36b6112558
Chris Eichler <chriseichler@gmail.com>
2013-09-05 13:54:24 -0400

    made jquery resizing work basically, and resizing back at over 800, by adjusting the width and left values of divs

--- portfolio.js
+++ portfolio.js
@@ -15,4 +15,29 @@ $(function() {
   });
 });
 
+$(window).resize(function(){
+  if ($(window).width() <= 800){  
+    // do something here
+    $('#title_section').css("width", "78%");
+    $('#title_section').css("left", "11%");
+    $('#containere').css("left", "18%");
+  } 
+});
+
+$(window).resize(function(){
+  if ($(window).width() >= 800){  
+    // do something here
+    $('#title_section').css("width", "40%");
+    $('#title_section').css("left", "8%");
+    $('#containere').css("left", "13%");
+  } 
+});
+
+// $(window).resize(function(){
+//   if ($(window).width() >= 800){  
+//     // do something here
+//     $('#title_section').remove('<br />');
+//   } 
+// });
+
   // $(#containerf).append('<br>')
